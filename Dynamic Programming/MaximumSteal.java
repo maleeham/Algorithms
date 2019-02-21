5cfa0f3ae92781294919711f4cc7481a6d1c0978
Maleeha <msk180001@utdallas.edu>
2019-02-21 12:28:50 -0600

    Update MaximumSteal.java

--- Dynamic Programming/MaximumSteal.java
+++ Dynamic Programming/MaximumSteal.java
@@ -1,34 +1,37 @@
 // Java program to find the maximum stolen value 
 import java.io.*; 
 
-class Algo 
+class Algo
 { 
 	// Function to calculate the maximum stolen value 
 	static int maxLoot(int hval[], int n) 
 	{ 
 		if (n == 0) 
 		return 0; 
+
+		int value1 = hval[0]; 
 		if (n == 1) 
-			return hval[0]; 
+			return value1; 
+
+		int value2 = Math.max(hval[0], hval[1]); 
 		if (n == 2) 
-			return Math.max(hval[0], hval[1]); 
-
-		// dp[i] represent the maximum value stolen 
-		// so far after reaching house i. 
-		int[] dp = new int[n]; 
-
-		// Initialize the dp[0] and dp[1] 
-		dp[0] = hval[0]; 
-		dp[1] = Math.max(hval[0], hval[1]); 
+			return value2; 
+	
+		// contains maximum stolen value at the end 
+		int max_val = 0; 
 
 		// Fill remaining positions 
-		for (int i = 2; i<n; i++) 
-			dp[i] = Math.max(hval[i]+dp[i-2], dp[i-1]); 
+		for (int i=2; i<n; i++) 
+		{ 
+			max_val = Math.max(hval[i]+value1, value2); 
+			value1 = value2; 
+			value2 = max_val; 
+		} 
 
-		return dp[n-1]; 
+		return max_val; 
 	} 
 	
-	// Driver program 
+	// driver program 
 	public static void main (String[] args) 
 	{ 
 		int hval[] = {6, 7, 1, 3, 8, 2, 4}; 
