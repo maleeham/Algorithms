7d0da4b3d3c1763ce2a49b9cba8a657d30645d1a
Maleeha <msk180001@utdallas.edu>
2019-02-21 13:12:32 -0600

    Update KaratsubaAlgorithm.java

--- Divide-and-Conquer/KaratsubaAlgorithm.java
+++ Divide-and-Conquer/KaratsubaAlgorithm.java
@@ -13,7 +13,6 @@ class Karatsuba {
         // number of bits divided by 2, rounded up
         N = (N / 2) + (N % 2);
 
-        // x = a + 2^N b,   y = c + 2^N d
         BigInteger b = x.shiftRight(N);
         BigInteger a = x.subtract(b.shiftLeft(N));
         BigInteger d = y.shiftRight(N);
