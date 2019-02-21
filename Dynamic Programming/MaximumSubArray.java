416640561bc2f6a334a1d188fbae91c1e96bb155
Maleeha <msk180001@utdallas.edu>
2019-02-21 12:23:13 -0600

    Update MaximumSubArray.java

--- Dynamic Programming/MaximumSubArray.java
+++ Dynamic Programming/MaximumSubArray.java
@@ -2,8 +2,6 @@
 This is an optimization problem, which can be usually solved by DP. So when it comes to DP, the first thing for us to figure out is the format of the sub problem(or the state of each sub problem). The format of the sub problem can be helpful when we are trying to come up with the recursive relation.
 */
 
-//Code for Max Sub Array function
-
 public int maxSubArray(int[] A) {
         int n = A.length;
         int[] dp = new int[n];        //dp[i] means the maximum subarray ending with A[i];
